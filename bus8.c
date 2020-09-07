d4ad6261f017e4e685246373f3574c1c4512916d
P3TCH <60442724+P3TCH@users.noreply.github.com>
2020-09-08 01:36:55 +0700

    Add files via upload

---
+++ bus8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//6309650395
+//Sorawit Namseetan
+
+  int main()
+  {
+    float speed,time;
+    speed = 4.2;
+
+    printf("Enter Time : ");
+    scanf("%f",&time);
+    printf("The distance is %0.10f meter.\n",speed*time);
+
+  }
